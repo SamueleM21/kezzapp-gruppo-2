fe58162ba90fd9f6bfcd23d920b9dae8af09cfe6
DESKTOP-J5C4BKM\Samuele <5>
2021-03-17 11:40:07 +0100

    creazione server e client
    creazione server e client

---
+++ kezzapp-server/src/main/java/due/gruppo/kezzapp/KezzappApplication.java
@@ -0,0 +1,13 @@
+package due.gruppo.kezzapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KezzappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KezzappApplication.class, args);
+	}
+
+}
---
+++ kezzapp-server/src/test/java/due/gruppo/kezzapp/KezzappApplicationTests.java
@@ -0,0 +1,13 @@
+package due.gruppo.kezzapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KezzappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
